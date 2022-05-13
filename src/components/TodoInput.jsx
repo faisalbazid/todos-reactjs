57bbe9134cc1e2e5c956df2a6c881b10db86bd6d
faisalbazid <91886338+faisalbazid@users.noreply.github.com>
2022-05-13 19:36:34 +0100

    Add files via upload
    update

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -8,13 +8,14 @@ const AddTaskForm = ({ newTask, setNewTask, addTask }) => {
             value={newTask}
             onChange={ (e) => setNewTask(e.target.value)}
             className="form-control form-control-lg"
+            placeholder="Add Todo..."
           />
         </div>
         <div className="col-auto">
           <button
             onClick={addTask}
             className="btn btn-lg btn-success"
-          >Add Task</button>
+          >+</button>
         </div>
       </div>
       <br />
